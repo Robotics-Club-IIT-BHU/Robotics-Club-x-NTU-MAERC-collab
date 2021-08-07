350aaa1af7b8c9402dfac87c124888e8551ef1e8
ajjuu <72931799+aju22@users.noreply.github.com>
2021-08-07 19:57:35 +0530

    Update Forward_Kinematics.py

--- Day_2/Kinematics/Forward_Kinematics.py
+++ Day_2/Kinematics/Forward_Kinematics.py
@@ -4,22 +4,22 @@ import os
 import time
 import math
 
-file_name = "2R_Robotic_Arm.urdf"
+file_name = "2R_Robotic_Arm.urdf"  # Should be present in the same directory of this code.
 p.connect(p.GUI)
 p.loadURDF(os.path.join(pybullet_data.getDataPath(), "plane.urdf"), 0, 0, 0)
 orn = p.getQuaternionFromEuler([0, 0, 0])
 robot = p.loadURDF(file_name, [0, 0, 0], orn)
-p.createConstraint(parentBodyUniqueId=robot, parentLinkIndex=0, childBodyUniqueId=-1,
+p.createConstraint(parentBodyUniqueId=robot, parentLinkIndex=0, childBodyUniqueId=-1,         # For fixing the robot base_plate to origin.
                    childLinkIndex=-1, jointType=p.JOINT_POINT2POINT, jointAxis=[1, 0, 0],
                    parentFramePosition=[0, 0, 0], childFramePosition=[0, 0, 0])
 p.setGravity(0, 0, -10)
 
-l1 = 1  # update the length of link-1,from the urdf
-l2 = 1  # update the length of link-2,from the urdf
+l1 = 1  # the length of link-1,from the urdf
+l2 = 1  # the length of link-2,from the urdf
 
 
 def Forward_Kinematics(angle_1, angle_2):
-
+    # Since we are working in Y-Z plane, respective equations give the y and z cordinates of the end-effector, for the corresponding angle_1 and angle_2.
     global l1, l2
     y = (l1 * math.cos(angle_1)) + (l2 * math.cos((angle_1 + angle_2)))
     z = (l1 * math.sin(angle_1)) + (l2 * math.sin(angle_1 + angle_2))
