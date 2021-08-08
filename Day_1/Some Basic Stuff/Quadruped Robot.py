43b1610aca2b9f15fcd31eaece98efb6624a0979
harshmahesheka <78502324+harshmahesheka@users.noreply.github.com>
2021-08-08 12:29:17 +0530

    Add files via upload

---
+++ Day_1/Some Basic Stuff/Quadruped Robot.py
@@ -0,0 +1,16 @@
+import pybullet as p
+import time
+import pybullet_data
+physicsClient = p.connect(p.GUI)#or p.DIRECT for non-graphical version
+p.setAdditionalSearchPath(pybullet_data.getDataPath()) #optionally
+p.setGravity(0,0,0)
+planeId = p.loadURDF("plane.urdf")
+StartPos = [0,0,1]
+StartOrientation = p.getQuaternionFromEuler([0,0,0])
+boxId = p.loadURDF("Quadruped Robot.urdf",StartPos, StartOrientation)
+for i in range (10000):
+    p.stepSimulation()
+    time.sleep(1./240.)
+cubePos, cubeOrn = p.getBasePositionAndOrientation(boxId)
+print(cubePos,cubeOrn)
+p.disconnect()
