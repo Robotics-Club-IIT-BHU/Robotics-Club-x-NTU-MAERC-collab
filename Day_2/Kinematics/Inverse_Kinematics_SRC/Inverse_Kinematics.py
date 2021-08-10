5250ed2062bb388c03d4e25001ecba70477f226d
ajjuu <72931799+aju22@users.noreply.github.com>
2021-08-10 16:41:11 +0530

    Update Inverse_Kinematics.py

--- Day_2/Kinematics/Inverse_Kinematics_SRC/Inverse_Kinematics.py
+++ Day_2/Kinematics/Inverse_Kinematics_SRC/Inverse_Kinematics.py
@@ -9,7 +9,7 @@ p.connect(p.GUI)
 p.loadURDF(os.path.join(pybullet_data.getDataPath(), "plane.urdf"), 0, 0, 0)
 orn = p.getQuaternionFromEuler([0, 0, 0])
 robot = p.loadURDF(file_name, [0, 0, 0], orn)
-p.createConstraint(parentBodyUniqueId=robot, parentLinkIndex=0, childBodyUniqueId=-1,
+p.createConstraint(parentBodyUniqueId=robot, parentLinkIndex=0, childBodyUniqueId=-1,       # Fixing the robot in place.
                    childLinkIndex=-1, jointType=p.JOINT_POINT2POINT, jointAxis=[1, 0, 0],
                    parentFramePosition=[0, 0, 0], childFramePosition=[0, 0, 0])
 
@@ -18,12 +18,12 @@ l2 = 1
 
 def Inverse_kinematics(target):
     global l1, l2
-    z, y = target
+    y, z = target
 
     angle_2 = math.acos(((y ** 2 + z ** 2) - l1 ** 2 - l2 ** 2) / (2 * l1 * l2))
-    angle_1 = math.atan(y / z) - (math.atan((l2 * math.sin(angle_2)) / (l1 + l2 * math.cos(angle_2))))
+    angle_1 = math.atan(z / y) - (math.atan((l2 * math.sin(-angle_2)) / (l1 + l2 * math.cos(-angle_2))))
 
-    return angle_1, angle_2
+    return angle_1, -angle_2
 
 
 # Equation of circle we will be using (y-1)^2 + (z-0.5)^2 = (0.5)^2
@@ -33,19 +33,20 @@ r = 0.5
 y_old = 1.5
 z_old = 0.5
 
-use_custom = False  # Switch to True, for using custom Inverse_Kinematics function.
+use_custom = False
 
 while True:
 
-    y_new = r * math.cos(theta) + 1
+    y_new = r * math.cos(theta) + 1       # Cartesian to Polar Cordinate Transformation
     z_new = r * math.sin(theta) + 0.5
 
     if use_custom:
-        angle_1, angle_2 = Inverse_kinematics([z_new, y_new])
+        angle_1, angle_2 = Inverse_kinematics([y_new, z_new])
     else:
-        angle_1, angle_2 = p.calculateInverseKinematics(robot, 2, [0, y_new, z_new])
+        rotation = p.getQuaternionFromEuler([0, math.pi, 0]) # For rotated configuration of the arm
+        angle_1, angle_2 = p.calculateInverseKinematics(robot, 2, [0, y_new, z_new], rotation)
 
-    p.addUserDebugLine([0, y_old, z_old], [0, y_new, z_new], [1,0,0])
+    p.addUserDebugLine([0, y_old, z_old], [0, y_new, z_new], [1,0,0])   # For Visualization
 
     y_old = y_new
     z_old = z_new
